c95bf9e9951e4c6c29a2e83ffb158b1937c34544
Dgonede <dgonegonee@gmail.com>
2024-10-02 12:54:14 +0300

    add cover

--- homework_01/tests/test_log_analyzer.py
+++ homework_01/tests/test_log_analyzer.py
@@ -26,3 +26,27 @@ def test_analyze_response_times():
     assert analysis_results['median'] == 0.456
     assert analysis_results['max'] == 0.789
     assert analysis_results['min'] == 0.123    
+
+
+def test_parse_nginx_logs_insufficient_elements():
+    mock_log_data = (
+        '127.0.0.1 - - [01/Jan/2023:00:00:01 +0000] "GET / HTTP/1.1" 200 0.123\n'
+        '127.0.0.1 - - [01/Jan/2023:00:00:02 +0000] "GET /about HTTP/1.1" 200\n'  # Нет времени отклика
+    )
+
+    with patch('builtins.open', mock_open(read_data=mock_log_data)):
+        response_times = parse_nginx_logs('dummy_log_file.txt')
+        expected_response_times = pd.Series([0.123])
+        pd.testing.assert_series_equal(response_times, expected_response_times)
+
+
+def test_parse_nginx_logs_invalid_response_time():
+    mock_log_data = (
+        '127.0.0.1 - - [01/Jan/2023:00:00:01 +0000] "GET / HTTP/1.1" 200 abc\n'
+        '127.0.0.1 - - [01/Jan/2023:00:00:02 +0000] "GET /about HTTP/1.1" 200 0.456\n'
+    )
+    with patch('builtins.open', mock_open(read_data=mock_log_data)):
+        response_times = parse_nginx_logs('dummy_log_file.txt')
+        expected_response_times = pd.Series([0.456])
+        pd.testing.assert_series_equal(response_times, expected_response_times)
+
